a33fe8761c4c9febcae3ea35bc4118d8e8b2bd5e
ernesto4664 <ernesto4664@gmail.com>
2024-09-18 16:30:32 -0300

    mejorando cosas para realizar despliegue

---
+++ src/app/admin/settings/page.tsx
@@ -0,0 +1,9 @@
+// src/app/admin/settings/page.tsx
+export default function SettingsPage() {
+    return (
+      <div>
+        <h1>Configuraciones del Admin</h1>
+        {/* Aquí iría el contenido de la página */}
+      </div>
+    );
+  }
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+// src/app/blog/page.tsx
+export default function BlogPage() {
+    return (
+      <div>
+        <h1>Bienvenido a mi Blog</h1>
+        <p>Aquí compartiré artículos y novedades sobre desarrollo web y tecnología.</p>
+      </div>
+    );
+  }
+  
